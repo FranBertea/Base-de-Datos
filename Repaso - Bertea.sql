b7cc0f5e01b5c6d86cedfca80e4a83533d8446c6
FranBertea <80895162+FranBertea@users.noreply.github.com>
2021-06-20 14:19:22 -0300

    Add files via upload

---
+++ Repaso - Bertea.sql
@@ -0,0 +1,11 @@
+Create database Producto; 
+Use Producto; 
+Create Table Items (
+IdProducto int primary key not null,
+Nombre varchar (25) not null,
+Precio float not null,
+Stock int not null
+); 
+Show tables; 
+Show columns from Items;
+
