7ce4ff7df829054c6280ace32c32b19d29cd5f92
FranBertea <80895162+FranBertea@users.noreply.github.com>
2021-07-08 09:43:07 -0300

    Add files via upload

--- 29 de Junio - Bertea.sql
+++ 29 de Junio - Bertea.sql
@@ -15,6 +15,18 @@ Curso varchar(40) not null
 SHOW COLUMNS FROM ESTUDIANTES; 
 
 SELECT idest, Nombre, Apellido, Nota, Curso FROM ESTUDIANTES; 
+ 
+INSERT INTO ESTUDIANTES (idest,nombre, apellido, nota, curso) values ("1", "Juan","Perez", "81","A");
+INSERT INTO ESTUDIANTES (idest,nombre, apellido, nota, curso) values ("2", "Sol","Gonzalez", "102","B");
+INSERT INTO ESTUDIANTES (idest,nombre, apellido, nota, curso) values ("3", "Daniel","Aramburu", "63","A");
+INSERT INTO ESTUDIANTES (idest,nombre, apellido, nota, curso) values ("4", "Jose","Lopez", "41","B");
+INSERT INTO ESTUDIANTES (idest,nombre, apellido, nota, curso) values ("5", "Marta","Caceres", "52","A");
+INSERT INTO ESTUDIANTES (idest,nombre, apellido, nota, curso) values ("6", "Ivan","Suarez", "75","A");
+INSERT INTO ESTUDIANTES (idest,nombre, apellido, nota, curso) values ("7", "Ruben","Pasteur", "35","B");
+INSERT INTO ESTUDIANTES (idest,nombre, apellido, nota, curso) values ("8", "Pablo","Pagani", "96","B");
+INSERT INTO ESTUDIANTES (idest,nombre, apellido, nota, curso) values ("9", "Susana","Romero", "86","A");
+INSERT INTO ESTUDIANTES (idest,nombre, apellido, nota, curso) values ("10", "Maria","Lujan", "104","A");
+SELECT *FROM ESTUDIANTES;
 
 
 
